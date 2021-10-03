0dfb20b4dc56c12549b6dc9b68e04e9edffd7683
rpgarde <rpgarde@gmail.com>
2021-10-03 23:19:09 +1100

    updated .env files

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,11 +1,22 @@
+require('dotenv').config();
+
+const express = require("express");
 const mongoose = require('mongoose');
 const db = require('../models');
 
-mongoose.connect('mongodb://localhost/workout', {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 const workoutSeed = [
   {
